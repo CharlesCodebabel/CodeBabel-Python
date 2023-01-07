7691e594934f892e62687d2d4e977209267d7415
CharlesCodeBabel <charles.code.babel@gmail.com>
2023-01-07 16:48:47 -0300

    CBBLDoc : new file with IBGE Data

---
+++ PyCEP/CodeBabelPyCepIBGE.py
@@ -0,0 +1,54 @@
+#((( API ))) : requests
+#CodeBabelPayCep
+
+"""
+https://cep.awesomeapi.com.br/:format/:cep
+( end point ) https://cep.awesomeapi.com.br/json/05424020
+
+atualização : 30s
+resp : 200 cep encontrado,
+resp : 400 cep inválido
+resp : 404 Cep não encontrado ((error))
+"""
+
+import requests
+import json
+
+#
+print('\n<CodeBabelPyCEP : Somente números>\n')
+get_cep = int(input('Digite o CEP : '))
+ep = "https://cep.awesomeapi.com.br/json/{}".format(get_cep) #End Point
+req = requests.get(ep)  #Obter resposta 220 : ok, 404 : erro.
+# Requisição print(req)
+
+consulta = req.json()
+
+#colect data/coletando dados
+
+cep_data = consulta['cep']
+cep_address = consulta['address']
+cep_state = consulta['state']
+cep_district = consulta['district']
+cep_city = consulta['city']
+cep_city_ibge = consulta['city_ibge']
+cep_lat = consulta['lat']
+cep_lng = consulta['lng']
+cep_ddd = consulta['ddd']
+
+#exit__
+print('CEP : {}\nEndereço : {}\nEstado : {}\nBairro : {}\nCidade : {}\nIBGE : {}\nLatitude : {}\nLongitude : {}\nDDD : {}'.format(cep_data,cep_address,cep_state,cep_district,cep_city,cep_city_ibge,cep_lat,cep_lng,cep_ddd))
+print('\nInformações e panorama : IBGE\n')
+
+#colect state/district__
+IBGES = cep_state
+IBGEC = cep_district
+
+#general infos__
+print('[INFORMAÇÕES GERAIS]:')
+inf = ('https://www.ibge.gov.br/cidades-e-estados/{}/{}.html'.format(IBGES,IBGEC))
+print(inf)
+
+#panoram__
+print('\n[PANORAMA]:')
+pan = ('https://cidades.ibge.gov.br/brasil/{}/{}/panorama'.format(IBGES,IBGES))
+print(pan)
