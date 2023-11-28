41abc1187bfd8d824db316f5d3c2c93ee2986e23
Charles Rodrigues <100997012+CharlesCodebabel@users.noreply.github.com>
2023-11-28 16:14:34 -0300

    Update CodeBabelPyCep.py
    Refactory code, pythonic...

--- PyCEP/CodeBabelPyCep.py
+++ PyCEP/CodeBabelPyCep.py
@@ -1,8 +1,6 @@
-#((( API ))) : requests
-#CodeBabelPyCep
+#((( API ))) : requests > CodeBabelPyCep
 
 """
-https://cep.awesomeapi.com.br/:format/:cep
 ( end point ) https://cep.awesomeapi.com.br/json/05424020
 
 atualização : 30s
@@ -11,43 +9,33 @@ resp : 400 cep inválido
 resp : 404 Cep não encontrado ((error))
 """
 
-import requests
-import json
+import requests, json
 
-#
 print('\n<CodeBabelPyCEP : Somente números>\n')
 get_cep = int(input('Digite o CEP : '))
-ep = "https://cep.awesomeapi.com.br/json/{}".format(get_cep) #End Point
-req = requests.get(ep)  #Obter resposta 220 : ok, 404 : erro.
-# Requisição print(req)
-
+ep = "https://cep.awesomeapi.com.br/json/{}".format(get_cep) # End Point
+req = requests.get(ep)  # [ 200 : ok, 404 : error ] --> verify requisition ... print(req)
 consulta = req.json()
 
 #colect data/coletando dados
+cep_data = consulta['cep']; cep_address = consulta['address']
+cep_state = consulta['state']; cep_district = consulta['district']
+cep_city = consulta['city']; cep_city_ibge = consulta['city_ibge']
+cep_lat = consulta['lat']; cep_lng = consulta['lng']; cep_ddd = consulta['ddd']
 
-cep_data = consulta['cep']
-cep_address = consulta['address']
-cep_state = consulta['state']
-cep_district = consulta['district']
-cep_city = consulta['city']
-cep_city_ibge = consulta['city_ibge']
-cep_lat = consulta['lat']
-cep_lng = consulta['lng']
-cep_ddd = consulta['ddd']
+# exit__
+print('CEP : {}\nEndereço : {}\nEstado : {}\nBairro : {}\nCidade : {}',
+      '\nIBGE : {}\nLatitude : {}\nLongitude : {}\nDDD : {}'.format(cep_data,
+                                                                    cep_address,cep_state,
+                                                                    cep_district,cep_city,
+                                                                    cep_city_ibge,cep_lat,
+                                                                    cep_lng,cep_ddd)
+)
 
-#exit__
-print('CEP : {}\nEndereço : {}\nEstado : {}\nBairro : {}\nCidade : {}\nIBGE : {}\nLatitude : {}\nLongitude : {}\nDDD : {}'.format(cep_data,cep_address,cep_state,cep_district,cep_city,cep_city_ibge,cep_lat,cep_lng,cep_ddd))
+# colect state/district__
+IBGES = cep_state; IBGEC = cep_district
+INF = ('https://www.ibge.gov.br/cidades-e-estados/{}/{}.html'.format(IBGES,IBGEC))
+PAN = ('https://cidades.ibge.gov.br/brasil/{}/{}/panorama'.format(IBGES,IBGES))
 
-#colect state/district__
-IBGES = cep_state
-IBGEC = cep_district
-
-#general infos__
-print('[INFORMAÇÕES GERAIS]:')
-inf = ('https://www.ibge.gov.br/cidades-e-estados/{}/{}.html'.format(IBGES,IBGEC))
-print(inf)
-
-#panoram__
-print('\n[PANORAMA]:')
-pan = ('https://cidades.ibge.gov.br/brasil/{}/{}/panorama'.format(IBGES,IBGES))
-print(pan)
+# general infos__
+print('[INFORMAÇÕES GERAIS]: \n{}\n[PANORAMA]: \n{}'.format(INF, PAN))
